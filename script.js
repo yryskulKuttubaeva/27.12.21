c78edc1b847a00000917799d37cbe57f87bd37e1
yryskul <kuttubaevayryskul@gmail.com>
2021-12-27 14:33:30 +0600

    added return

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-function callFriend(friendsName =""){
+function callFriend(friendsName ="Aizu"){
     console.log(friendsName);
     console.log('Pick up the phone');
     console.log('Type in the number of' + friendsName);
     console.log('Press call');
     console.log('Wait for the answer');
 }
-callFriend("Aizu");
-callFriend("Mali");
-callFriend("Eliza");
-callFriend();
+
+// callFriend("Aizu");
+// callFriend("Mali");
+// callFriend("Eliza");
+// callFriend();
+
+function calculareSquareArea(a,b) {
+    console.log(a * b);
+}
+
+// calculareSquareArea(5, 100000);
+// calculareSquareArea(10, 12);
+// calculareSquareArea(1, 0);
+
+function calculteCylinderVolume(r, h) {
+    return 3.14 * r * r * h;
+}
+var volume = calculteCylinderVolume(3, 4);
+// console.log(volume);
+
+var smallGlassVol = calculteCylinderVolume(3,4);
+var bottleVol = calculteCylinderVolume (3, 10);
+
+// console.log(smallGlassVol);
+// console.log(bottleVol);
+
