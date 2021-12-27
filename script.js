3c81c64cedb01e7e7c3e3e50a4e77891113eb635
yryskul <kuttubaevayryskul@gmail.com>
2021-12-27 14:19:37 +0600

    added function

---
+++ script.js
@@ -0,0 +1,11 @@
+function callFriend(friendsName =""){
+    console.log(friendsName);
+    console.log('Pick up the phone');
+    console.log('Type in the number of' + friendsName);
+    console.log('Press call');
+    console.log('Wait for the answer');
+}
+callFriend("Aizu");
+callFriend("Mali");
+callFriend("Eliza");
+callFriend();
